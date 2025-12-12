99334de1b48fbc0b7ebc16b42bbc37ce00ddddac
veeresh betageri1718 <betageriveeresh17@gmail.com>
2025-12-12 09:14:14 +0530

    Create math.py

---
+++ math.py
@@ -0,0 +1,8 @@
+try:
+    a = float(input("Enter First number: "))
+    b = float(input("Enter Second number: "))
+
+    print(f"Addition of two numbers: {a + b}")
+    print(f"Subtraction of two numbers: {a - b}")
+except Exception as e:
+    print(f"An error occurred: {e}")
